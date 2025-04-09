bc90335b480b70318374beac5d97a92679633501
Javier Gonzalez <ja.gonzalezv@outlook.com>
2025-04-09 13:00:19 -0600

    - Removed auth token from version control

--- app/src/main/java/com/challenge/movies/util/Constans.kt
+++ app/src/main/java/com/challenge/movies/util/Constans.kt
@@ -2,5 +2,5 @@ package com.challenge.movies.util
 
 object Constants {
     const val ITEMS_PER_PAGE = 20
-    const val AUTH_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWM2MDllMjE4MTA1ZGQzYWZlMmI0YmJjZjRjZWJiYSIsIm5iZiI6MTc0NDA2NDE4Mi4yNiwic3ViIjoiNjdmNDRlYjY4Y2ZjY2Y3YmFmZDkyNDllIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.vegIf19HHv3oJM2POY9L3CpH0LYg74jC32-6fSkkWWw"
+    const val AUTH_TOKEN = "{YOUR_AUTH_TOKEN}"
 }
